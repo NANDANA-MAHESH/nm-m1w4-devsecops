17202e7e4075fba7eeacf5741f19bd3213065ad5
Nandana Mahesh <116829670+NANDANA-MAHESH@users.noreply.github.com>
2026-09-16 20:22:44 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -18,3 +18,23 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
+
+resource "aws_s3_bucket" "devsecops_lab" {
+  bucket = "nm-mahesh-devsecops-lab-2026-001"
+
+  tags = {
+    Owner       = "Nandana"
+    Environment = "Lab"
+    CostCentre  = "Cyber"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "devsecops_lab" {
+  bucket = aws_s3_bucket.devsecops_lab.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
