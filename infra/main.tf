0af7e9582adbda5697984f7162f98c5842ae4ff3
Nandana Mahesh <116829670+NANDANA-MAHESH@users.noreply.github.com>
2026-09-17 05:20:36 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -29,12 +29,3 @@ resource "aws_s3_bucket" "devsecops_lab" {
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "devsecops_lab" {
-  bucket = aws_s3_bucket.devsecops_lab.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
