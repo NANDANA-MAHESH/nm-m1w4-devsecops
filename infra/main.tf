624cba00933618878a75c5a847a4235fe1fe5edb
Nandana Mahesh <116829670+NANDANA-MAHESH@users.noreply.github.com>
2026-09-17 05:00:01 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -29,12 +29,6 @@ resource "aws_s3_bucket" "devsecops_lab" {
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "devsecops_lab" {
-  bucket = aws_s3_bucket.devsecops_lab.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
+resource "aws_s3_bucket" "bad_bucket" {
+  bucket = "nm-hard-mandatory-test-001"
 }
